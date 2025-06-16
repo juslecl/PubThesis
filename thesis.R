8e764a59b951b8464fcf37964339acc01e427f6c
juslecl <justine.leclerc@uzh.ch>
2025-06-16 11:14:36 +0200

    1606

--- thesis.R
+++ thesis.R
@@ -30,7 +30,7 @@ rmod3 <- function(n, g, s) {
 #-------------------------------------------------------------
 
 #1) Create a dataset with X following a normal but different patterns for the error.
-datagen <- function(n, param, rcdf, g, meanX, sdX,  M=100) {
+datagen <- function(n, param, rcdf, g, meanX, sdX,  M=1000) {
   beta <- param[-length(param)]
   s <- param[length(param)]
   d <- length(meanX)
@@ -60,38 +60,40 @@ ols <- function(data) {
 neg_loglik <- function(param, data, g, type = 1) { #param is a vector of length(beta)+1
   t <- (data$Y-data$X%*%param[1:(length(param)-1)])/param[length(param)]
   s <- param[length(param)]
+  print(s)
   
   if (type == 1) {
-    # Type 1: f(t) = d * exp(-c * |t|^gamma)
+    # Type 1: f(t) = d/s * exp(-c * |t|^gamma)
     c_gamma <- (gamma(3 / g) / gamma(1 / g))^(g/ 2)
     d_gamma <- (g/ 2) * (gamma(3 / g) / gamma(1 / g)^3)^(1/2)
     log_f <- -log(s) + log(d_gamma) - c_gamma * abs(t)^g
     
   } else if (type == 2) {
-    # Type 2: f(t) = d * |t|^{gamma - 1} * exp(-c * |t|), gamma ≥ 1
+    # Type 2: f(t) = d/s * |t|^{gamma - 1} * exp(-c * |t|), gamma ≥ 1
     c_gamma <- (gamma(g + 2) / gamma(g))^(1/2)
     d_gamma <- (1 / (2 * gamma(g))) * (gamma(g+ 2) / gamma(g))^(g/ 2)
     log_f <- -log(s) + log(d_gamma) + (g - 1) * log(abs(t)) - c_gamma * abs(t)
     
   } else if (type == 3) {
-    # Type 3: f(t) = d * |t|^{gamma - 1} * exp(-c * |t|^gamma), gamma ≥ 1
+    # Type 3: f(t) = d/s * |t|^{gamma - 1} * exp(-c * |t|^gamma), gamma ≥ 1
     c_gamma <- gamma(2 / g + 1)^(g / 2)
     d_gamma <- (g / 2) * gamma(2 / g + 1)^(g / 2)
     log_f <- -log(s) + log(d_gamma) + (g - 1) * log(abs(t)) - c_gamma * abs(t)^g
   }
-  
   return(-sum(log_f))
 }
 
 # Function to compute MLE of beta. 
 compute_mle_beta <- function(data, g, type = 1) {
+  ols_est <- ols(data)
+  print(ols_est)
+  s_start <- sqrt(var(data$Y - data$X%*%ols_est))
+  print(s_start)# or MAD-based
   neg_loglik_wrapped <- function(param) {
     neg_loglik(param, data = data, g = g, type = type)
   }
-  
-  res <- optimx(par = c(rep(0, ncol(data$X)),1), fn = neg_loglik_wrapped, method = "BFGS")
-  
-  resvec <- res[, 1:(ncol(data$X)+1)]  # fixed res -> res
+  res <- optimx(par = c(as.numeric(ols_est), s_start), fn = neg_loglik_wrapped, method = "BFGS")
+  resvec <- res[, 1:(ncol(data$X)+1)]
   return(mle=t(resvec))
 }
 
@@ -103,6 +105,7 @@ mleols <- function(data, g, type){
   res <- bptry({bplapply(seq_len(M), function(nb){
     mle <- compute_mle_beta(data[[nb]], g, type)
     olse <- c(ols(data[[nb]]),NA)
+    print(data[[nb]]$P)
     return(cbind.data.frame(mle=mle,ols=olse,truth=data[[nb]]$P))
   },BPPARAM=param)})
   return(res)
@@ -119,7 +122,7 @@ metrics <- function(mleols_res){
     norm2mle <- norm(dati[,1]-dati[,3],type="2")
     return(list(norm2ols,norm2mle))
   },BPPARAM=param)})
-  norms <- matrix(unlist(norms),nrow=100,ncol=2,byrow=T)
+  norms <- matrix(unlist(norms),nrow=M,ncol=2,byrow=T)
   colnames(norms) <- c("OLSE","MLE")
   empirical_means <- vector("list", 2)
   empirical_vars  <- vector("list", 2)
@@ -145,9 +148,9 @@ beta <- c(sample(seq(from=-3,to=3,by=.01),d),sample(seq(from=1,to=3,by=.01),1))
 meanX <- sample(seq(from=-5,to=5,by=.01),d)
 sdX <- sample(seq(from=0,to=2,by=.1),d)
 
-data11 <- datagen(n, beta, rmod1 , 3, meanX, sdX, M=100)
-data12 <- datagen(n, beta, rmod2 , 3, meanX, sdX, M=100)
-data13 <- datagen(n, beta, rmod3 , 3, meanX, sdX, M=100)
+data11 <- datagen(n, beta, rmod1 , 3, meanX, sdX)
+data12 <- datagen(n, beta, rmod2 , 3, meanX, sdX)
+data13 <- datagen(n, beta, rmod3 , 3, meanX, sdX)
 
 mleols11 <- mleols(data11, 3, 1)
 mleols12 <- mleols(data12, 3, 2)
@@ -156,3 +159,6 @@ mleols13 <- mleols(data13, 3, 3)
 metrics11 <- metrics(mleols11)
 metrics12 <- metrics(mleols12)
 metrics13 <- metrics(mleols13)
+
+# GRAPHS
+boxplot(metrics11[[1]],main="Euclidean norm to the true coefficients'vector")
